6baf7a8eb2185a46fec0ce1c9f240ab362e5457f
john <john@hesse>
2013-10-07 23:26:14 +0200

    wrap internal exceptions

---
+++ src/main/java/de/h7r/beandiff/BeanDiffRuntimeException.java
@@ -0,0 +1,10 @@
+package de.h7r.beandiff;
+
+public class BeanDiffRuntimeException
+        extends RuntimeException {
+
+    public BeanDiffRuntimeException(Exception e) {
+        super (e);
+    }
+
+}
